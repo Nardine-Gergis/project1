8dc087ad837469ee63b568e1e728d474a624d19b
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-23 16:59:36 -0400

    starting set up of html and js

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    
+    //data base where information is stored
+    //a playlist; (e.g. https://openwhyd.org/adrien/playlist/61)
+      var queryURL = 
+      var queryURL2 = 
+
+
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+       }).then(function (response) {
+
+       };
+
+       $.ajax({
+        url: queryURL2,
+        method: "GET"
+       }).then(function (response) {
+       }
