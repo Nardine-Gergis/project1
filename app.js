91c57b4ec56cc9a8e3b65ba4ea8b964c86f8f0ad
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-24 15:54:14 -0400

    update

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     //data base where information is stored
-    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+x+"&key=AIzaSyBb9SLVx_0mHhYCPqnaZJjEX1Ii-iHEuuE";
-    var apiKey1 = "AIzaSyBb9SLVx_0mHhYCPqnaZJjEX1Ii-iHEuuE"
+    //var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+x+"&key=AIzaSyBb9SLVx_0mHhYCPqnaZJjEX1Ii-iHEuuE";
     var apiKey2 = "d2b6366945a9a0c064fc4b1be7fadb4a932e099f"
     var queryURL2 = "https://wger.de/api/v2/exercise/?muscles=1&equipment=3";
-    var x = "Cardio"
+    var x = ""
     function youVideo(x){
+    
      $.ajax({
-        url: queryURL,
+        url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+x+"&key=AIzaSyBb9SLVx_0mHhYCPqnaZJjEX1Ii-iHEuuE",
         method: "GET"
      }).then(function (response) {
         console.log(response)
