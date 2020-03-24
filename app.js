d489e66c834781c960294e9cca721bf15b24969f
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-23 22:20:31 -0400

    api keys, basic html

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-$(document).ready(function() {
-    
-    //data base where information is stored
-    //a playlist; (e.g. https://openwhyd.org/adrien/playlist/61)
-      var queryURL = 
-      var queryURL2 = 
+$(document).ready(function () {
+               //data base where information is stored
+    var queryURL = ("");
+      //var queryURL2 =  ("");
+        //youtube api key = AIzaSyBb9SLVx_0mHhYCPqnaZJjEX1Ii-iHEuuE
+        //wger api key = d2b6366945a9a0c064fc4b1be7fadb4a932e099f
 
 
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-       }).then(function (response) {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response)
 
-       };
-
-       $.ajax({
-        url: queryURL2,
-        method: "GET"
-       }).then(function (response) {
-       }
+        });
+ 
+   });      
