7ef834acf1927123673f0f311a75cd4e2d48e482
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-25 15:26:32 -0400

    page response complete

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
     var queryURL2 = "https://wger.de/api/v2/exerciseinfo/?page=5";
     var x = ""
     exerciseinfo();
+
+    //function that runs the first call
     function youVideo(x){
     
      $.ajax({
@@ -15,13 +17,64 @@ $(document).ready(function () {
         console.log("https://www.youtube.com/watch?v="+response.items[0].id.videoId)
         var a = $("<a>").attr("href", "https://www.youtube.com/watch?v="+response.items[0].id.videoId)
 
-        window.location.href = "https://www.youtube.com/watch?v="+response.items[0].id.videoId;
+    
+        window.open("https://www.youtube.com/watch?v="+response.items[0].id.videoId, "_blank")
  
      });
     }
+        //event listener for endurance
        $("#endurance").on("click", function(){
          youVideo("cardio")
        })
+       //event listener for hiit
+       $("#Hiit").on("click", function(){
+        youVideo("hiit")
+      })
+      //event listener for low intensity cardio
+      $("#low").on("click", function(){
+        youVideo("running")
+      })
+      //event listener for zumba
+      $("#zumba").on("click", function(){
+        youVideo("dance workout")
+      })
+      //event listener for chest
+      $("#chest").on("click", function(){
+        youVideo("chest workout")
+      })
+      //event listener for back
+      $("#back").on("click", function(){
+        youVideo("back workout")
+      })
+      //event listener for legs
+      $("#legs").on("click", function(){
+        youVideo("leg workout")
+      })
+      //event listener for abs
+      $("#abs").on("click", function(){
+        youVideo("abs workout")
+      })
+      //event listener for karma
+      $("#karma").on("click", function(){
+        youVideo("karma yoga")
+      })
+      //event listener for zumba
+      $("#bhakti").on("click", function(){
+        youVideo("bhakti yoga")
+      })
+      //event listener for zumba
+      $("#jnana").on("click", function(){
+        youVideo("jnana yoga")
+      })
+      //event listener for zumba
+      $("#raja").on("click", function(){
+        youVideo("raja yoga")
+      })
+      //event listener for zumba
+      $("#calisthenics").on("click", function(){
+        youVideo("calisthenics workout")
+      })
+
         function exerciseinfo(){
     $.ajax({
         url: queryURL2,
