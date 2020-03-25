ce33dbf09a7ac89ff1c9b2f15bf3162ef4a98f91
Steven Leonardo <stevenleonardo@knights.ucf.edu>
2020-03-25 13:50:04 -0400

    list redirects

--- app.js
+++ app.js
@@ -11,21 +11,23 @@ $(document).ready(function () {
         url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+x+"&key=AIzaSyBb9SLVx_0mHhYCPqnaZJjEX1Ii-iHEuuE",
         method: "GET"
      }).then(function (response) {
-        //console.log(response)
+        console.log(response)
         console.log("https://www.youtube.com/watch?v="+response.items[0].id.videoId)
         var a = $("<a>").attr("href", "https://www.youtube.com/watch?v="+response.items[0].id.videoId)
-        //assign to page and append
-        $("#cardio").append(a)
+
+        window.location.href = "https://www.youtube.com/watch?v="+response.items[0].id.videoId;
  
      });
     }
-        youVideo("cardio");
+       $("#endurance").on("click", function(){
+         youVideo("cardio")
+       })
         function exerciseinfo(){
     $.ajax({
         url: queryURL2,
         method: "GET"
     }).then(function (response) {
-        console.log(response)
+        //console.log(response)
 
     })
     };
